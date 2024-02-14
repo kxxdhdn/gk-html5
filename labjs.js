151de00f4688fa62628a861f559faf13eeb2fca4
kxxdhdn <zayintopology@gmail.com>
2024-02-14 23:23:00 +0100

    AutoSave@Zaira-20240214232252

---
+++ labjs.js
@@ -0,0 +1,72 @@
+function somme(a, b) {
+    return a + b;
+}
+
+function calculerSomme() {
+    let x = prompt("Saisissez le premier nombre?");
+    x = parseFloat(x);
+    let y = prompt("Saisissez le deuxième nombre?");
+    y = parseFloat(y);
+
+    let result = somme(x, y);
+
+    document.write("La somme de " + x + " et " + y + " est de " + result);
+}
+
+function somme() {
+    var a = document.getElementById("input1").value;
+    var b = document.getElementById("input2").value;
+    var somme = Number(a) + Number(b)
+
+    document.getElementById("result").value = somme;
+}
+
+function valider() {
+    var nomInput = document.forms["cinForm"]["nom"].value;
+    var cinInput = document.forms["cinForm"]["cin"].value;
+
+    // Check vide
+    if (nomInput === "" || cinInput === "") {
+        alert("Veuillez remplir tous les champs !");
+        return;
+    }
+    // Check CIN
+    if (!(/^\d{8}$/.test(cinInput))) {
+        alert("CIN doit être un nombre de 8 digits !");
+        return;
+    }
+    // Valide
+    alert("Valide ！");
+    document.forms["cinForm"]["nom"].value = "";
+    document.forms["cinForm"]["cin"].value = "";
+}
+
+function choisir() {
+    var checked = [];
+    var checkboxes = document.forms["checkForm"].querySelectorAll("input[type='checkbox']:checked");
+
+    checkboxes.forEach(function(checkbox) {
+        checked.push(checkbox.value);
+    });
+
+    if (checked.length === 0) {
+        alert("Il faut pas ne rien maîtriser !");
+    } else {
+        alert("Vous avez maîtrisé : " + checked.join(", "));
+    }
+}
+
+function jouer() {
+    var score = 0;
+    var checkboxes = document.forms["gameForm"];
+    
+    for (var checkbox of checkboxes) {
+        if (checkbox.checked) {
+            score += parseInt(checkbox.value);
+        }
+    };
+                       
+
+    // document.getElementById("score").textContent = `Score du jeu: ${score}`;
+    document.getElementById("score").value = score;
+}
